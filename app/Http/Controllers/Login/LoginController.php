ac2c46feb768e62e1c1223e6e8a6e0ba794a46a6
losers <losers@localhost.localdomain>
2019-05-14 15:49:03 +0800

    key

--- app/Http/Controllers/Login/LoginController.php
+++ app/Http/Controllers/Login/LoginController.php
@@ -26,7 +26,7 @@ class LoginController    extends Controller
         //解密
         $data = file_get_contents('php://input');
         $enc_data=base64_decode($data);
-        $pk=openssl_get_publickey('file://'.storage_path('app/keys/public.pem'));
+        $pk=openssl_get_publickey('file://keys/public.pem');
         openssl_public_decrypt($enc_data,$dec_data,$pk);
         $data=json_decode($dec_data,true);
         $info_table=DB::table('user')->where(['email'=>$data['email']])->first();
